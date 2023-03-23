1b609aeae100d943026852723b15b0e32e89c479
dutysniper <natarov.constantin@yandex.ru>
2023-03-24 00:58:55 +0300

    Update ViewController.swift
    create Outlets and Actions

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redValue: UILabel!
+    @IBOutlet weak var greenValue: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var shape: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeRedValue(_ sender: Any) {
+    }
+    @IBAction func changeGreenValue(_ sender: Any) {
+    }
+    @IBAction func changeBlueValue(_ sender: Any) {
+    }
 }
 
