998931b6d764956b97b446b139f12dccfd0c4f52
dutysniper <natarov.constantin@yandex.ru>
2023-03-24 17:57:14 +0300

    update view corner radius

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -21,6 +21,7 @@ final class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        shape.layer.cornerRadius = 15
         setColor()
     }
     
